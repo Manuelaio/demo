0525e152befe4105a3443a3399250b36fa109497
eiovino <emanuelaiovino.87@gmail.com>
2020-06-29 20:56:07 +0200

    rerun

--- tests/mytest.R
+++ tests/mytest.R
@@ -1,13 +1,7 @@
 app <- ShinyDriver$new("../")
 app$snapshotInit("mytest")
 
-app$uploadFile(gene1 = "mygene.txt", wait_=FALSE, values_=FALSE)
-Sys.sleep(30)
+app$uploadFile(gene1 = "mygene.txt", wait_=FALSE, values_=FALSE) # <-- This should be the path to the file, relative to the app's tests/ directory
 app$snapshot()
 app$setInputs(pileup = "click", wait_=FALSE, values_=FALSE)
-app$snapshot()
-app$setInputs(Gene_name = "POLG", wait_=FALSE, values_=FALSE)
-app$setInputs(tabSet = "UCSC gene", wait_=FALSE, values_=FALSE)
-app$setInputs(button1 = "click",timeout_=1000000)
-Sys.sleep(30)
 app$snapshot()
